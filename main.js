7fb411db64e466ca47e2ab70f217b34d62fc57e1
JacfarMohaz <jacfarmahamed77@gmail.com>
2023-12-06 12:33:12 +0300

    adding contact page

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const menuIcon = document.querySelector("#menu-icon")
 const closeIcon = document.querySelector("#close-icon")
 const ulList = document.querySelector("ul")
 
-menuIcon.addEventListener("click", function() {
+menuIcon.addEventListener("click", function () {
     menuIcon.style.display = "none"
     closeIcon.style.display = "block"
     ulList.style.display = "block"
@@ -11,7 +11,7 @@ menuIcon.addEventListener("click", function() {
     ulList.style.paddingRight = "15px"
 })
 
-closeIcon.addEventListener("click", function() {
+closeIcon.addEventListener("click", function () {
     menuIcon.style.display = "block"
     closeIcon.style.display = "none"
     ulList.style.display = "none"
@@ -21,29 +21,39 @@ closeIcon.addEventListener("click", function() {
 // banner image changed 5 sec
 
 document.addEventListener("DOMContentLoaded", function () {
-            // Array of image URLs
-            const imageUrls = [
-                'assets/Rectangle 7 (7).png',
-                'assets/Rectangle 7 (8).png',
-                'assets/MTJV3.png',
-                'assets/MT253.png',
-                'assets/Rectangle 7 (4).png',
-                'assets/Rectangle 7 (6).png',
-                // Add more image URLs as needed
-            ];
+    // Array of image URLs
+    const imageUrls = [
+        'assets/Rectangle 7 (7).png',
+        'assets/Rectangle 7 (8).png',
+        'assets/MTJV3.png',
+        'assets/MT253.png',
+        'assets/Rectangle 7 (4).png',
+        'assets/Rectangle 7 (6).png',
+        // Add more image URLs as needed
+    ];
 
-            // Get the image element
-            const imageElement = document.getElementById('changeableImage');
+    // Get the image element
+    const imageElement = document.getElementById('changeableImage');
 
-            // Function to change the image
-            function changeImage() {
-                // Get a random index to select a random image from the array
-                const randomIndex = Math.floor(Math.random() * imageUrls.length);
+    // Function to change the image
+    function changeImage() {
+        // Get a random index to select a random image from the array
+        const randomIndex = Math.floor(Math.random() * imageUrls.length);
 
-                // Set the new image source
-                imageElement.src = imageUrls[randomIndex];
-            }
+        // Set the new image source
+        imageElement.src = imageUrls[randomIndex];
+    }
 
-            // Change the image every 5 seconds in a loop
-            setInterval(changeImage, 5000);
-        });
+    // Change the image every 5 seconds in a loop
+    setInterval(changeImage, 5000);
+});
+
+
+const deleteIcon = document.querySelector("#delete-icon")
+const card = document.querySelector("#card")
+
+deleteIcon.addEventListener("click", () => {
+    card.style.display = "none"
+    
+})
+
